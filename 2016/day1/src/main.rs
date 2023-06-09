8d42a37f1c49b81ced749bfd6eb289ede14d03e1
pylbrecht <pylbrecht@mailbox.org>
2023-06-09 14:07:49 +0200

    Parse input day1

--- 2016/day1/src/main.rs
+++ 2016/day1/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq)]
+struct Instruction {
+    direction: Direction,
+    distance: usize,
+}
+
+fn parse_input(input: &str) -> Vec<Instruction> {
+    input
+        .split(", ")
+        .map(|inst| {
+            let (direction, distance) = inst.split_at(1);
+            let direction = match direction {
+                "R" => Direction::Right,
+                "L" => Direction::Left,
+                _ => panic!("unknown direction"),
+            };
+            let distance = usize::from_str(distance).unwrap();
+            Instruction {
+                direction,
+                distance,
+            }
+        })
+        .collect()
+}
+
+fn main() { }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let input = "R2, L3";
+        let directions = vec![
+            Instruction {
+                direction: Direction::Right,
+                distance: 2,
+            },
+            Instruction {
+                direction: Direction::Left,
+                distance: 3,
+            },
+        ];
+        let parsed = parse_input(input);
+        assert_eq!(parsed, directions);
+    }
 }
